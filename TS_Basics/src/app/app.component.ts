2a79d8d3ea70231d8e93a49aa0e914830f41f68a
MaheshSumb <maheshsumbpatil87@gmail.com>
2025-07-22 23:32:03 +0530

    TS Basics Day 2 Conditionals- If else , if-elseif-else , switch , and Looping - For , while , do while , foreach

--- TS_Basics/src/app/app.component.ts
+++ TS_Basics/src/app/app.component.ts
@@ -19,6 +19,113 @@ export class AppComponent {
     let n = this.ff(5)
     console.log(n);
 
+
+
+    // Day 2 TS Basics
+    console.log("====================")
+    console.log("Conditionals");
+
+
+    let isValid: boolean = true
+    if (isValid) {
+      console.log('Valid......')
+    }
+
+    let gender = 'male'
+    if (gender == 'mail') {
+      console.log('Gender is Male')
+    }
+    else if (gender = 'female') {
+      console.log('Gender is Female')
+    }
+    else {
+      console.log("Gender is other");
+
+    }
+    console.log("====================")
+    console.log("====================")
+    console.log("Switch");
+
+    let day: string = 'suntay'
+    switch (day) {
+      case 'sunday': console.log("sunday")
+        break
+      case 'monday':
+        console.log('monday')
+        break
+      case 'Tuesday':
+        console.log('Tuesday')
+        break
+      default:
+        console.log('other')
+
+
+    }
+    console.log("====================")
+
+    console.log("====================")
+    console.log("Looping");
+
+    console.log("For");
+
+    // For
+    for (let i = 0; i < 10; i++) {
+      console.log(i)
+
+    }
+
+    console.log("While");
+    // While
+    let j = 10;
+    while (j <= 20) {
+      console.log(j)
+      j++
+    }
+
+    // Do while
+
+    console.log("Do While");
+    let k = 20;
+    do {
+      console.log(k)
+      k--;
+    } while (k >= 10)
+
+    // For each
+
+    let arr = ['ram', 'sham', 'radha']
+    console.log("For Each");
+
+    for (let i of arr) {
+      console.log(i)
+    }
+    arr.forEach(element => {
+      console.log(element)
+    });
+
+    let p = [{
+      id: 101,
+      name: 'ram',
+      age: 21
+    }, {
+      id: 102,
+      name: "sham",
+      age: 25
+    },
+    {
+      id: 103,
+      name: 'radha',
+      age: 29
+    }
+    ]
+
+    for (let i of p) {
+      console.log(i.id, i.name, i.age)
+
+    }
+    console.log("====================")
+
+
   }
 
   // Declaration of variable
@@ -67,3 +174,5 @@ export class AppComponent {
 
 
 }
+
+
